68bd1a746fde2bb4640bdfc0fc7fd7a64657586c
Jason Wong <wongjas@gmail.com>
2014-03-14 00:20:36 -0700

    cartesian.py: Initial attempt at closest cartesian problem

---
+++ cartesian.py
@@ -0,0 +1,32 @@
+#/usr/bin/python
+
+import sys
+import math
+
+class Point():
+    """Represents a cartesian coordinate with x and y"""
+
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        
+    def __str__(self):
+        return ''.join(["(", str(self.xpos), ", ",str(self.ypos),")"])
+
+    def __eq__(self, other):
+        return self.xpos == other.xpos and self.ypos == other.ypos
+
+    def shortestDistance(self):
+        dist = math.pow(self.xpos**2 + self.ypos**2, 0.5)
+        self.dist = dist
+        return self
+
+# ListOfPoints -> ListOfPoints
+def threeClosest(points):
+    [sys.stdout.write(p) for p in points]
+    ptsWithDist= [p.shortestDistance() for p in points]
+    ptsWithDist.sort()
+    return ptsWithDist[0:2]
+    
+if __name__ == '__main__':
+    threeClosest()
---
+++ cartesian_test.py
@@ -0,0 +1,22 @@
+#/usr/bin/python
+
+import unittest
+from cartesian import *
+
+class TestCartesian(unittest.TestCase):
+    
+    def setUp(self):
+        self.point1 = Point(2, 3)
+        self.point2 = Point(3, 4)
+        self.point3 = Point(4, 5)
+        self.point4 = Point(2, 7)
+        self.point5 = Point(5, 10)
+        self.list = [self.point1, self.point2, self.point3, 
+                     self.point4, self.point5]
+
+    def testCartesian(self):
+        self.assertEqual(threeClosest(self.list),
+                         [self.point1, self. point2, self.point3])
+
+if __name__ == '__main__':
+    unittest.main()
